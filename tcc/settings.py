42eb9e37f7a83f6b37f17c2cef874e1d665f7fd4
jens_bekersch <jens.bekersch@googlemail.com>
2022-10-23 18:23:23 +0200

    * added django-environ and .env.template

--- tcc/settings.py
+++ tcc/settings.py
@@ -1,20 +1,20 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
 from django.contrib import staticfiles
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@fj$vbhtw&i4fn&!lq+hi7czp_hc0=tne1a%zchqn30*-#qe7k'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
